19502d609fcb008dc75e94db173b29ba31b9d810
rwparrish <60946750+rwparrish@users.noreply.github.com>
2022-03-24 22:21:27 -0600

    Final

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+    return ( 
+        <>
+            <button onBlur={ () => {console.log('Hey! Eyes on me!')} } onFocus={() => {console.log('Good!')} } >Eyes on me.
+            </button>
+        </>
+     );
+}
+
+export default EyesOnMe;
